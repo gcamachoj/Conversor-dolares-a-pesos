be7e3502f65c3bf6984544247e89b69d48f387d8
gcamachoj <guiller.camacho.jara@gmail.com>
2021-08-27 13:05:38 -0500

    Primer commit, programa basico, hasta ahora solo consta de un archivo

---
+++ conversor.py
@@ -0,0 +1,19 @@
+print("==============================")
+print(" CONVERSOR DE DOLARES A PESOS")
+print("==============================")
+dolares = input("Cuántos dolares quieres convertir a pesos colombianos?")
+dolares = int(dolares)
+valor_dolar = 3865.5
+pesos_co = dolares*valor_dolar
+pesos_co=float(pesos_co)
+pesos_co=round(pesos_co,2)
+pesos_co=str(pesos_co)
+dolares=str(dolares)
+print("")
+print("* Haz confirmado tener $: " + dolares + " dolares")
+print("* tu cantidad en pesos CO es $" + pesos_co + " pesos(co)")
+print()
+
+
+
+
